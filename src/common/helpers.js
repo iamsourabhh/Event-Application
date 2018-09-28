26fafced6b7390fa031393adf58ce4362ad000a1
iamsourabhh <iamsourabhh@gmail.com>
2018-09-28 23:19:50 +0530

    helper functin fixed

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -5,7 +5,11 @@ export const getTimeFromInteger = i => {
   if (i < 12) {
     return i + " AM";
   }
-  if (i >= 12 && i < 24) {
-    return i + " PM";
+  if (i > 12 && i < 24) {
+    return (i % 12) + " PM";
+  }
+
+  if (i === 12) {
+    return "12 PM";
   }
 };
